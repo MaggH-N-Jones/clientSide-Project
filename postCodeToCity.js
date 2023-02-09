5fad23b73a9b3ee29acf3b2af4839cf2b422b815
JonasNJensen <102585691+JonasNJensen@users.noreply.github.com>
2023-02-09 13:37:42 +0100

    renamed files

--- postCodeToCity.js
+++ postCodeToCity.js
@@ -1,7 +1,15 @@
 async function postCodeToCity(){
-    document.getElementById("kommune").innerHTML = "Hello World";
-    document.getElementById("city").innerHTML = "Hello World";
 
-    var postnr = document.getElementById("city").value
+    let postnr = document.getElementById("post-nr").value;
+    
     fetch('https://api.dataforsyningen.dk/postnumre/' + postnr)
-}
+    .then (Response=>{
+        return Response.json();
+    })
+    .then(json=>{
+        document.getElementById("city").innerHTML = json.navn;
+        document.getElementById("kommune").innerHTML = json.kommuner.navn;
+    })
+    
+    
+}   
