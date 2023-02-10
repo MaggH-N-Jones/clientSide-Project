fe8cae659f0c867b88e763623d95a13b424a8ce9
MaggH-Jones <123364051+MaggH-N-Jones@users.noreply.github.com>
2023-02-10 09:25:31 +0100

    big chungus

--- postCodeToCity.js
+++ postCodeToCity.js
@@ -1,15 +1,21 @@
-async function postCodeToCity(){
+let searchButton = document.getElementById("search-button");
 
-    let postnr = document.getElementById("post-nr").value;
-    
-    fetch('https://api.dataforsyningen.dk/postnumre/' + postnr)
-    .then (Response=>{
-        return Response.json();
-    })
-    .then(json=>{
-        document.getElementById("city").innerHTML = json.navn;
-        document.getElementById("kommune").innerHTML = json.kommuner.navn;
-    })
-    
-    
-}   
+function kommuneToHTML(kommune) {
+    return '<p>' + kommune.navn + '</p>'
+
+}
+
+/** @returns {Promise<string[]>} */
+async function fetchkommuner(postnummer) {
+    let response = await fetch('https://api.dataforsyningen.dk/postnumre/' + postnummer);
+    let data = await response.json();
+    return data.kommuner;
+}
+
+searchButton.addEventListener("click", async () => {
+    let postnummer = document.getElementById("post-nr").value;
+    let kommuner = await fetchkommuner(postnummer);
+    let kommuneElements = kommuner.map(kommuneToHTML);
+    let kommuneElementsjoind = kommuneElements.join('');
+    document.getElementById("kommune").innerHTML = kommuneElementsjoind;
+})
